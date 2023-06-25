842cac60294f1a360e4115f19b6d3cbc5d36f638
AXUSLK <thilina.lyceum@gmail.com>
2023-06-25 23:31:18 +0530

    💄 Footer and Favicon Icon Added

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,11 @@
+<section class="bg-gray-100">
+    <footer
+        class="bottom-0 w-full mx-auto py-10 text-center text-sm text-gray-500 sm:flex sm:items-center sm:justify-center">
+        <p>&copy; {{ Carbon\Carbon::now()->format('Y') }} Online Book Store App All Rights Reserved.
+            Made with <span class="text-red-500">❤️</span> by
+            <a class="hover:text-teal-600" target="_blank" href="https://saradabhagya.me/" rel="noopener noreferrer">
+                <strong>Sarada Bhaya</strong>
+            </a>
+        </p>
+    </footer>
+</section>
